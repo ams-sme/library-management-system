86b1f1b283193f67830d892c5582285ded7c563a
AayushMS <mansinghaayush@gmail.com>
2023-06-27 07:12:31 +0545

    first commit

---
+++ models/transactions.py
@@ -0,0 +1,7 @@
+class Transactions():
+    
+    def __init__(self, book, borrower, tenure, return_date):
+        self.book = book
+        self.borrower = borrower
+        self.tenure = tenure
+        self.return_date = return_date
